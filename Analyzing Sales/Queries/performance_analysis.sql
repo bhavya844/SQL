2559dd26ee5d4403527c42dee309f7a07f044775
bhavya844 <bhavyadave700@gmail.com>
2025-03-11 19:06:14 -0300

    one more query for performance analysis

--- Analyzing Sales/Queries/performance_analysis.sql
+++ Analyzing Sales/Queries/performance_analysis.sql
@@ -38,7 +38,28 @@ when total_sales = round(avg(total_sales) over (partition by product_name),2) th
 when total_sales < round(avg(total_sales) over (partition by product_name),2) then 'below average'
 end compare_to_average
 from new_table
-order by order_year;
+order by product_name,order_year;
+
+# now let us compare the total_sales with the previous year sales. for this also we will
+# be using the window functions and using the lag() function in it.
+
+with new_table as (
+select year(order_date) as order_year, sum(sales_amount) as total_sales,
+product_name from `gold.fact_sales` f
+left join `gold.dim_products` p
+on f.product_key = p.product_key
+where order_date is not null
+group by product_name, order_year
+order by order_year asc
+) select *,
+lag(total_sales,1,0) over (partition by product_name order by order_year) as previous_year_sales,
+case when total_sales > lag(total_sales,1,0) over (partition by product_name order by order_year) then 'greater'
+when total_sales = lag(total_sales,1,0) over (partition by product_name order by order_year) then 'equal'
+when total_sales < lag(total_sales,1,0) over (partition by product_name order by order_year) then 'lesser'
+end compare_previous_year
+from new_table
+order by product_name, order_year;
+
 
 
 
