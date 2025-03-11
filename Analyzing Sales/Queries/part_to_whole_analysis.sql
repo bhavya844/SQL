da56bfb91a3b43b58cf4b64279cd859c83c5645f
bhavya844 <bhavyadave700@gmail.com>
2025-03-11 19:13:43 -0300

    add business problem

---
+++ Analyzing Sales/Queries/part_to_whole_analysis.sql
@@ -0,0 +1,9 @@
+# here we are going to do part-to-whole analysis
+
+# here we are going to understand how an individual part is performing compared to the 
+# overall. This allows us to understand which category has the greatest impact  on the 
+# business
+
+# formula is ([Measure]/Total[Measure]) * 100 By [Dimension]
+
+# the task is to find out which categories contribute the most to the overall sales
