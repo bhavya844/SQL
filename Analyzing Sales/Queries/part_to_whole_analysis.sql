f4d47d33489f0f45eac3d1e2119b546cbcf221ff
bhavya844 <bhavyadave700@gmail.com>
2025-03-11 19:33:05 -0300

    add query for part_to_whole analysis

--- Analyzing Sales/Queries/part_to_whole_analysis.sql
+++ Analyzing Sales/Queries/part_to_whole_analysis.sql
@@ -7,3 +7,16 @@
 # formula is ([Measure]/Total[Measure]) * 100 By [Dimension]
 
 # the task is to find out which categories contribute the most to the overall sales
+
+with new_table as (
+select sales_amount, category from `gold.dim_products` d
+right join `gold.fact_sales` f
+on d.product_key = f.product_key
+) ,new_table_2 as (
+select *, sum(sales_amount) over (partition by category) as total_sum_category
+from new_table)
+select *, sum(sales_amount) over () as final_sum,
+concat((total_sum_category)/(sum(sales_amount) over ()) * 100, '%') as percentage 
+from new_table_2
+order by percentage desc;
+
