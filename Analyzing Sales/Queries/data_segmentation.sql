f28e466591558dfdd02405bae54e28cafb6fe4d0
bhavya844 <bhavyadave700@gmail.com>
2025-03-11 21:16:51 -0300

    add query for data segmentation

--- Analyzing Sales/Queries/data_segmentation.sql
+++ Analyzing Sales/Queries/data_segmentation.sql
@@ -20,10 +20,34 @@ from new_table
 group by  cost_range
 order by count(product_name) desc;  
 
-# now we are going to start another task which is grouping the customers based on their 
-# spending behaviour. 
-# - so we have the VIP customers: at least 12 months of history and spending more than 5000
-# - Regular cusotmer at least 12 months of history but spending 5000 or less
-# - New cusotmers with lifespan less than 12 months
+/* now we are going to start another task which is grouping the customers based on their 
+spending behaviour. 
+- so we have the VIP customers: at least 12 months of history and spending more than 5000
+- Regular cusotmer at least 12 months of history but spending 5000 or less
+- New cusotmers with lifespan less than 12 months
+and find the total number of customers by each group 
+*/
+select * from `gold.fact_sales`;
+select * from `gold.dim_customers`;
+
+with new_table as (
+select f.customer_key, 
+sum(sales_amount) as total_sales, 
+max(order_date) as max_order_date, min(order_date) as min_order_date ,
+timestampdiff(month, min(order_date), max(order_date)) as total_months
+from `gold.fact_sales` f
+join `gold.dim_customers` d
+on f.customer_key = d.customer_key
+group by f.customer_key
+order by f.customer_key asc),
+new_table_2 as (select * , 
+case when total_sales > 5000 and total_months>= 12 then 'VIP'
+when total_sales<=5000 and total_months>=12 then 'Regular'
+else 'New' 
+end class_of_customers
+from new_table) 
+select class_of_customers, count(customer_key) from new_table_2
+group by class_of_customers ;
+
 
 
