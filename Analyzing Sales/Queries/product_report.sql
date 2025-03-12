9c3f184ad5d88e49bc80e7027c3cd07bd832f864
bhavya844 <bhavyadave700@gmail.com>
2025-03-12 18:43:32 -0300

    add requirements for product report

---
+++ Analyzing Sales/Queries/product_report.sql
@@ -0,0 +1,17 @@
+# here we are going to build the product report
+
+/* Purpose: The report consolidates key product metrics and behaviours
+
+Highlights:
+	1) Gathers essential fields such as product_name, category, subcategory and cost.
+    2) Segments products by revenue to identify High-performers, Mid_range, or Low-Performers.
+    3) Aggregates product-level metrics
+		- total orders
+        - total sales
+        - total quantity sold
+        - total customers (unique)
+        - lifespan (in months)
+	4) Calculates valuable KPIs
+		- recency (months since last sale)
+        - average order revenue (AOR)
+        - average monthly revenue
