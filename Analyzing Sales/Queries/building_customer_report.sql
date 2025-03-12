fe141d3fcf4971d4a4cce310151bf45e795e5711
bhavya844 <bhavyadave700@gmail.com>
2025-03-11 21:24:06 -0300

    adding the requirements of the customer report

---
+++ Analyzing Sales/Queries/building_customer_report.sql
@@ -0,0 +1,20 @@
+/* building a customer report 
+
+Purpose of the report-
+  This report should consolidate key customer metrics and behaviours
+  
+  Highlights: 
+  1) Gathers essential fields such as names, ages and transaction details.
+  2) Segments customers into categories (VIP, Regular, New) and age groups
+  3) Aggregates customer level metrics
+		- total orders
+        - total sales
+        - total quantity purchased
+        - total products
+        - lifespan (in months)
+4) Calculates valuable KPIs:
+		- recency(months since last order)
+        - average value order
+        - average monthly spend
+        */
+
