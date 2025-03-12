8b8e2230594e862c2ee7a793fe737df76bf8662e
bhavya844 <bhavyadave700@gmail.com>
2025-03-11 23:59:20 -0300

    adding the first query

--- Analyzing Sales/Queries/building_customer_report.sql
+++ Analyzing Sales/Queries/building_customer_report.sql
@@ -18,3 +18,25 @@ Purpose of the report-
         - average monthly spend
         */
 
+## selecting columns from the tables
+
+with new_table as (
+select f.order_number, f.product_key,
+f.order_date, f.sales_amount, f.quantity,
+c.customer_key, c.customer_number,  
+concat(c.first_name,' ',c.last_name) as name,
+timestampdiff(year, c.birthdate, curdate()) as age
+from `gold.fact_sales` f
+left join `gold.dim_customers` c 
+on f.customer_key =c.customer_key
+where order_date is not null)
+select customer_key, customer_number,  
+name, age, 
+sum(sales_amount) as total_sales,
+sum(quantity) as total_quantity,
+count(distinct product_key) as total_products,
+max(order_date) as max_date,
+timestampdiff(year, min(order_date), max(order_date)) as lifespan
+from new_table
+group by `name`, customer_key, age, customer_number;
+
