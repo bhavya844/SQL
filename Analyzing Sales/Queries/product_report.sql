10f3ae7bbfc29bcb010766687c3ab6d6f687117e
bhavya844 <bhavyadave700@gmail.com>
2025-03-12 19:15:36 -0300

    complete product report

--- Analyzing Sales/Queries/product_report.sql
+++ Analyzing Sales/Queries/product_report.sql
@@ -15,3 +15,51 @@ Highlights:
 		- recency (months since last sale)
         - average order revenue (AOR)
         - average monthly revenue
+*/
+
+select * from `gold.fact_sales`;
+select * from `gold.dim_customers`;
+select * from `gold.report_products`;
+with new_table as (
+select order_number, f.product_key, order_date, customer_key,
+sales_amount, quantity, product_name, category, subcategory,
+cost from `gold.fact_sales` f
+left join `gold.report_products` r
+on f.product_key  = r.product_key
+where order_date is not null),
+new_table_2 as (
+select product_key, product_name, category, subcategory, cost,
+timestampdiff(month, min(order_date), max(order_date)) as lifespan,
+max(order_date) as last_purchase,
+count(distinct order_number) as total_orders,
+count(distinct customer_key) as total_customers,
+sum(sales_amount) as total_sales,
+sum(quantity) as total_quantity,
+round(avg(cast(sales_amount as float)/ nullif(quantity,0)), 2) as avg_selling_price
+from new_table
+group by product_key, product_name, category, subcategory, cost)
+select product_key, product_name, category, subcategory, cost, last_purchase,
+timestampdiff(month, last_purchase, curdate()) as recency_in_months,
+case
+		when total_sales > 50000 then 'High-Performer'
+		when total_sales >= 10000 then 'Mid-Range'
+		else 'Low-Performer'
+	end as product_segment,
+	lifespan,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_customers,
+	avg_selling_price,
+case
+		when total_orders = 0 then 0
+		else total_sales / total_orders
+	end as avg_order_revenue,
+case
+		when lifespan = 0 then total_sales
+		else total_sales / lifespan
+	end as avg_monthly_revenue
+from new_table_2  ;
+
+
+
