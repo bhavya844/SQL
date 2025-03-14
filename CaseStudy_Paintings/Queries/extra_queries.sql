22e940270bcda352203f49b46d0249eac203540d
bhavya844 <bhavyadave700@gmail.com>
2025-03-14 19:00:40 -0300

    add some advanced queries

--- CaseStudy_Paintings/Queries/extra_queries.sql
+++ CaseStudy_Paintings/Queries/extra_queries.sql
@@ -75,8 +75,48 @@ group by country, city order by count(distinct(name)) desc;
 -- name, museum city and canvas label
 
 -- 20) Which country has the 5th highest no of paintings?
+select * from work;
+with new_table as (
+select m.museum_id, country, work_id, w.name from museum m
+right join work w
+on m.museum_id = w.museum_id
+where country is not null
+) , new_table_2 as 
+(select country, count(name) as total_paintings from new_table
+group by country order by count(name) desc),
+new_table_3 as (select country, total_paintings,
+row_number() over ( order by total_paintings desc) as `rank` 
+from new_table_2) 
+select * from new_table_3 where `rank`=5;
 
 -- 21) Which are the 3 most popular and 3 least popular painting styles?
 
+with most_popular as (
+select style, count(*) as total_count 
+from work where style is not null and museum_id is not null 
+group by style order by count(*) desc limit 3
+), least_popular as (
+select style, count(*) as total_count 
+from work where style is not null and museum_id is not null
+group by style order by count(*) asc limit 3
+) select style, total_count from most_popular
+union 
+select style, total_count from least_popular;
+
+
 -- 22) Which artist has the most no of Portraits paintings outside USA?. Display artist 
 -- name, no of paintings and the artist nationality.
+
+select * from museum;
+with new_table as (
+select a.artist_id, a.full_name, a.nationality, a.style, w.museum_id
+from artist a
+right join work w
+on a.artist_id = w.artist_id
+), new_table_2 as (
+select nt.museum_id, country, artist_id, full_name, nationality, style from new_table nt
+left join museum m on 
+nt.museum_id = m.museum_id
+where country not like '%USA%' and nt.museum_id is not null
+) select full_name, count(distinct(country)) as total_countries from new_table_2
+group by full_name order by count(distinct(country)) desc;
